7baf094a02313d7d7598125950968ba7d2323b83
Dhana7336 <157340298+Dhana7336@users.noreply.github.com>
2024-01-27 01:06:15 -0500

    Update MainActivity.java

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, " hello world mate !", Toast.LENGTH_LONG).show();
     }
 }
